086b23fa5458610217e63f2a1ec31fe9125fbf61
Liddy <liddy.pay@hotmail.com>
2016-02-04 12:36:25 -0700

    search layout

--- SchedulingApp/app/src/main/java/evolutionarydevelopment/schedulingapp/LoginActivity.java
+++ SchedulingApp/app/src/main/java/evolutionarydevelopment/schedulingapp/LoginActivity.java
@@ -207,6 +207,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
         }
     }
 
