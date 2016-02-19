be9ce982806ce5545bf677c9f83c786b49dd50a7
Liddy <liddy.pay@hotmail.com>
2016-02-19 14:11:39 -0700

    Home Page

--- Client/www/js/LoginController.js
+++ Client/www/js/LoginController.js
@@ -4,23 +4,13 @@
 
 app.controller('LoginController', function($scope, $http) {
 
-    /*$scope.signup = function() {
-
-      $http.post('http://localhost:3504/auth/signup', { email: $scope.email, password: $scope.password})
-        .success(function(data) {
-          console.log(data);
-        })
-        .error(function(err){
-          console.log(err);
-        });
-
-    }*/
-
   $scope.signin = function() {
 
       $http.post('http://localhost:3504/auth/login', { email: $scope.email, password: $scope.password})
         .success(function(data) {
           console.log(data);
+
+
         })
         .error(function(err){
           console.log(err);
