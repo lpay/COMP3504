6ca2e4f95122774bed75034fd48ccd778bb41054
Liddy <liddy.pay@hotmail.com>
2016-02-25 17:05:01 -0700

    Client Login & Logout

--- Client/www/js/HomeController.js
+++ Client/www/js/HomeController.js
@@ -2,10 +2,20 @@
  * Created by Liddy on 19-Feb-2016.
  */
 
-app.controller('HomeController', function($scope, $http) {
+app.controller("HomeController", function($scope, $http, $location, $auth) {
 
-  $scope.search = function() {
+  $scope.searchBar = function() {
 
 
-  }
+  };
+
+  $scope.logout =function (){
+    $auth.logout()
+      .then(function () {
+        $location.path('/login');
+        console.log("Logout Success!");
+      });
+  };
+
+
 });
