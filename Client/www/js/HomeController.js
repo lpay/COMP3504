344e02c86bcb73e176aeb0bfe7aaa30b7aa962b4
Liddy <liddy.pay@hotmail.com>
2016-03-09 19:41:28 -0700

    Layout changes (side menu + footer)

--- Client/www/js/HomeController.js
+++ Client/www/js/HomeController.js
@@ -2,28 +2,74 @@
  * Created by Johnny Admin on 2/29/2016.
  */
 
-app.controller('HomeController', function($scope, $http, $location, $auth) {
+app.controller('HomeController', function($scope, $http, $location, $auth, $ionicSideMenuDelegate, $ionicLoading) {
+
+  $scope.names = [];
+
+  $scope.doSearch = function() {
+    $scope.show($ionicLoading);
+    console.log('search');
+    $http.get('http://localhost:3504/events/' + encodeURIComponent($scope.search))
+      .success(function (data) {
+        console.log(data);
+        $scope.names = data;
+      })
+      .error(function (err) {
+
+      })
+    .finally(function ($ionicLoading) {
+      // On both cases hide the loading
+      $scope.hide($ionicLoading);
+    });
+  };
+
+  $scope.goToGroupSearch = function(){
+    $location.path("/groups");
+    console.log("Redirect Success!");
+  };
+
+  $scope.goToProfessionalSearch = function(){
+    $location.path("/users");
+    console.log("Redirect Success!");
+  };
 
 
-    $scope.names = [];
+  $scope.goToHome = function(){
+    $location.path("/");
+    console.log("Redirect Success!");
+  };
 
-    $scope.doSearch = function() {
-        console.log('search');
-        $http.get('http://localhost:3504/events/' + encodeURIComponent($scope.search))
-            .success(function (data) {
-                console.log(data);
-                $scope.names = data;
-            })
-            .error(function (err) {
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: '<p>Loading...</p><ion-spinner></ion-spinner>'
+    });
+  };
 
-            });
-    };
+  $scope.hide = function() {
+    $ionicLoading.hide();
+  };
 
-    $scope.logout =function (){
-        $auth.logout()
-            .then(function () {
-                $location.path('/login');
-                console.log("Logout Success!");
-            });
-    };
+  $scope.toggleLeft  = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  $scope.clearSearch = function() {
+    $scope.$parent.search = '';
+    $scope.names=[];
+    console.log("Clear Search");
+  };
+
+  $scope.toggleRight = function() {
+    $ionicSideMenuDelegate.toggleRight();
+  };
+
+  $scope.logout = function (){
+    $auth.logout()
+      .then(function () {
+        $location.path('/login');
+        console.log("Logout Success!");
+      });
+  };
+
 });
+
--- Client/www/js/app.js
+++ Client/www/js/app.js
@@ -5,88 +5,110 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('Client', ['ionic', 'satellizer'])
 
-    .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-        $stateProvider
+    $stateProvider
 
-            .state('login', {
-                url: '/login',
-                templateUrl: 'Login.html',
-                controller: 'LoginController'
-            })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'Login.html',
+        controller: 'LoginController'
+      })
 
-            .state('signup', {
-                url: '/signup',
-                templateUrl: 'Signup.html',
-                controller: 'SignupController'
-            })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'Signup.html',
+        controller: 'SignupController'
+      })
 
-            .state('home', {
-                url: '/',
-                templateUrl: 'Home.html',
-                controller: 'HomeController',
-                resolve: {
-                    loginRequired: loginRequired
-                }
-            });
-
-
-        function loginRequired($q, $location, $auth) {
-            var deferred = $q.defer();
-
-            if ($auth.isAuthenticated()) {
-                deferred.resolve();
-            } else {
-                $location.path('/login');
-                //$stateProvider.go('login');
-            }
-
-            return deferred.promise;
+      .state('home', {
+        url: '/',
+        templateUrl: 'Home.html',
+        controller: 'HomeController',
+        resolve: {
+          loginRequired: loginRequired
         }
+      })
 
-        $urlRouterProvider.otherwise('/');
-
-    })
-
-
-    .config(function($authProvider) {
-        var commonConfig = {
-            popupOptions: {
-                location: 'no',
-                toolbar: 'yes',
-                width: window.screen.width,
-                height: window.screen.height
-            }
-        };
-
-        if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
-            commonConfig.redirectUri = 'http://localhost:3504/';
+      .state('group', {
+        url: '/groups',
+        templateUrl: 'GroupSearch.html',
+        controller: 'GroupSearchController',
+        resolve: {
+          loginRequired: loginRequired
         }
+        })
 
-        $authProvider.loginUrl = 'http://localhost:3504/auth/login';
-        $authProvider.signupUrl = 'http://localhost:3504/auth/signup';
-
-        $authProvider.google(angular.extend({}, commonConfig, {
-            clientId: '870728536471-ilmvcb2obgo6ioucqokrgvcj211nj7t3.apps.googleusercontent.com',
-            url: 'http://localhost:3504/auth/google'
-        }));
-    })
-
-    .run(function($ionicPlatform) {
-
-        $ionicPlatform.ready(function() {
-            if(window.cordova && window.cordova.plugins.Keyboard) {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-                // Don't remove this line unless you know what you are doing. It stops the viewport
-                // from snapping when text inputs are focused. Ionic handles this internally for
-                // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
+          .state('users', {
+            url: '/users',
+            templateUrl: 'ProfessionalSearch.html',
+            controller: 'ProfessionalSearchController',
+            resolve: {
+              loginRequired: loginRequired
             }
-            if(window.StatusBar) {
-                StatusBar.styleDefault();
-            }
-        });
+
+      });
+
+    function loginRequired($q, $location, $auth) {
+      var deferred = $q.defer();
+
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      } else {
+        $location.path('/login');
+        //$stateProvider.go('login');
+      }
+
+      return deferred.promise;
+    }
+
+    $urlRouterProvider.otherwise('/');
+
+  })
+
+  .config(function($authProvider) {
+    var commonConfig = {
+      popupOptions: {
+        location: 'no',
+        toolbar: 'yes',
+        width: window.screen.width,
+        height: window.screen.height
+      }
+    };
+
+    if (ionic.Platform.isIOS() || ionic.Platform.isAndroid()) {
+      commonConfig.redirectUri = 'http://localhost:3504/';
+    }
+
+    $authProvider.loginUrl = 'http://localhost:3504/auth/login';
+    $authProvider.signupUrl = 'http://localhost:3504/auth/signup';
+
+    //$authProvider.locan(angular.extend({}, commonConfig, {
+    //  url: 'http://localhost:3504/auth/login'
+    //}));
+
+    $authProvider.google(angular.extend({}, commonConfig, {
+      clientId: '603122136500203',
+      url: 'http://localhost:3504/auth/google'
+    }));
+  })
+
+  .run(function($ionicPlatform) {
+
+
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
     });
+  });
