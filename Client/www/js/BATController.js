5944fb1fe9fcbeda07ceaaf12784b81fa9411d5c
Liddy <liddy.pay@hotmail.com>
2016-03-23 17:28:21 -0600

    BAT page

--- Client/www/js/BATController.js
+++ Client/www/js/BATController.js
@@ -1,3 +1,11 @@
 /**
  * Created by Johnny Admin on 3/22/2016.
  */
+app.controller('BATController', function($scope, $stateParams) {
+
+  $scope.member = $stateParams.member;
+  $scope.timeslot = $stateParams.timeslot;
+
+  console.log($scope.member);
+  console.log($scope.timeslot);
+});
